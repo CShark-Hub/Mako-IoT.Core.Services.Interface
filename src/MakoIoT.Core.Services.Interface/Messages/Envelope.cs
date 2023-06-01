a1897d0857f1cdd098b8d5510aa804b74ef68ae9
Bartek U <2597165+jazzman55@users.noreply.github.com>
2023-06-01 14:47:46 +0200

    move Messages to here

---
+++ src/MakoIoT.Core.Services.Interface/Messages/Envelope.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MakoIoT.Messages
+{
+    public class Envelope
+    {
+        public DateTime OriginTime { get; set; }
+        public string Sender { get; set; }
+        public string SenderAddress { get; set; }
+        public string MessageId { get; set; }
+        public IMessage Message { get; set; }
+    }
+}
